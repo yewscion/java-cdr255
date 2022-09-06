ad4fff50c48b9831c51fd53b13043503f83134cf
Christopher Rodriguez <yewscion@gmail.com>
2022-09-05 22:54:46 -0400

    Changes from Before Distraction.

--- cdr255/io/Input.java
+++ cdr255/io/Input.java
@@ -2,8 +2,20 @@ package cdr255.io;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class Input {
+    public static String readRawInput(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("\n");
+        String rawInput;
+        System.out.print(prompt);
+        rawInput = scanner.next();
+        scanner.close();
+        return rawInput;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello There!");
+        System.out.println(readRawInput("Tell Me Something."));
+    }
 }
 
 // Local Variables:
