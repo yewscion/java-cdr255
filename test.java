30c195cf3d8f09704b4eae4d0ad5f0aa00fc3a4e
Christopher Rodriguez <yewscion@gmail.com>
2022-11-01 20:20:35 -0400

    Dunno what this was, but it seems like it was important.

---
+++ test.java
@@ -0,0 +1,9 @@
+import cdr255.io.Input;
+
+public class test {
+    public static void main(String args[]) {
+        Input testing = new Input();
+        System.out.println(testing.readRawInput("Testing!"));
+    }
+}
+                           
